16936c23630e0ec23f7afe60843caa3e8882613b
Codecy2160 <kageroucsqc@gmail.com>
2025-01-15 14:48:42 +0800

    Frontend Tweaks

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-function scrollToAbout() { document.querySelector('#about').scrollIntoView({ behavior: 'smooth' }); }
+function scrollToAbout() {
+    const aboutElement = document.querySelector('#about');
+    const offsetTop = aboutElement.offsetTop;
+    window.scrollTo({ top: offsetTop - 20, behavior: 'smooth' }); // Adjust the 20 to the desired offset
+}
