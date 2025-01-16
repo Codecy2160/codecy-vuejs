93fc0ad73a4557de9faadc485a6e7e916fef3460
Codecy2160 <kageroucsqc@gmail.com>
2025-01-16 13:16:06 +0800

    Release

--- contact.js
+++ contact.js
@@ -1,24 +1,43 @@
 import axios from 'axios';
 
-export default {
-  data() {
-    return {
-      email: ''
+// export default {
+//   data() {
+//     return {
+//       email: ''
+//     }
+//   },
+//   methods: {
+//     sendEmail() {
+//       axios.post('codecyvt@gmail.com', {
+//         email: this.email
+//       })
+//       .then(response => {
+//         console.log(response);
+//         alert('Email sent successfully!');
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         alert('Error sending email!');
+//       });
+//     }
+//   }
+// }
+
+// All of that assuming I have a working email backend
+// Instead a pop up will display whenever the user submits an email
+
+const app = new Vue({
+    el: "app",
+    data: {
+      email: '',
+      showPopup: false
+    },
+    methods: {
+      sendEmail() {
+        this.showPopup = true;
+        setTimeout(() => {
+          this.showPopup = false;
+        }, 2000); 
+      }
     }
-  },
-  methods: {
-    sendEmail() {
-      axios.post('https://your-email-service.com/send-email', {
-        email: this.email
-      })
-      .then(response => {
-        console.log(response);
-        alert('Email sent successfully!');
-      })
-      .catch(error => {
-        console.error(error);
-        alert('Error sending email!');
-      });
-    }
-  }
-}
+  })
