963edbb445359801176bc12af6a7701b272d4c40
Codecy2160 <kageroucsqc@gmail.com>
2025-02-06 02:11:41 +0800

    supa BASED

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient('https:https://hpxyioljjbthzadttvjo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhweHlpb2xqamJ0aHphZHR0dmpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3NzgxNTEsImV4cCI6MjA1NDM1NDE1MX0.OQP-g_7lAwtpQ3vjZpg17RvHA1chCHoA7xYTUvdRnGc');
